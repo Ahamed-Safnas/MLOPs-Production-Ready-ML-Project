8a7f4fd25de0a99aa04afc541b41633e958a82d2
Safnas <m.safnas@cgiar.org>
2026-01-16 09:40:21 +0530

    adding initial files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="sfns",
+    author_email="safnas2042ahamed@gmail.com",
+    packages=find_packages()
+)
