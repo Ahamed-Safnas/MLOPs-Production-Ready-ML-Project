53c86419275857f4d948f3adb721ead9d86c52f1
safnasNED25 <safnas2042ahamed@gmail.com>
2026-01-22 19:37:27 +0530

    constants,entites were updated

--- us_visa/entity/artifact_entity.py
+++ us_visa/entity/artifact_entity.py
@@ -6,3 +6,8 @@ class DataIngestionArtifact:
     trained_file_path:str 
     test_file_path:str 
 
+@dataclass
+class DataValidationArtifact:
+    validation_status:bool
+    message: str # to show datadrift is there or not
+    drift_report_file_path: str # path to the drift report file
