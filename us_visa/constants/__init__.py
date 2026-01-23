3064d25f00b20f2855e5f99d9c618008fbad95c6
Ahamed Safnas <129087227+Ahamed-Safnas@users.noreply.github.com>
2026-01-23 16:16:50 +0530

    constants file updated

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -59,3 +59,15 @@ MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
 MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
 MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
+
+"""
+MODEL EVALUATION related constant 
+"""
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "usvisa-model2024"  #add your s3 bucket name here
+MODEL_PUSHER_S3_KEY = "model-registry"
+
